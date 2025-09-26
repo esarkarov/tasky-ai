1bbb980d228bef082c06cbe4dc37cad9297d1a13
Elvin <elvinsrkrov@gmail.com>
2025-09-26 15:03:42 +0400

    style: task card component created

--- src/pages/InboxPage.tsx
+++ src/pages/InboxPage.tsx
@@ -1,15 +1,20 @@
 import { Head } from '@/components/Head';
 import { Page, PageHeader, PageList, PageTitle } from '@/components/Page';
+import { TaskCard } from '@/components/TaskCard';
 import { TaskCardSkeleton } from '@/components/TaskCardSkeleton';
 import { TaskCreateButton } from '@/components/TaskCreateButton';
 import { TaskEmptyState } from '@/components/TaskEmptyState';
 import { TaskForm } from '@/components/TaskForm';
 import { TopAppBar } from '@/components/TopAppBar';
+import { Models } from 'appwrite';
 import { useState } from 'react';
-import { useFetcher } from 'react-router';
+import { useFetcher, useLoaderData } from 'react-router';
 
 const InboxPage = () => {
   const fetcher = useFetcher();
+  const { tasks } = useLoaderData<{
+    tasks: Models.DocumentList<Models.Document>;
+  }>();
   const [taskFormShow, setTaskFormShow] = useState(false);
 
   return (
@@ -24,6 +29,19 @@ const InboxPage = () => {
         </PageHeader>
 
         <PageList>
+          {tasks?.documents.map(
+            ({ $id, content, completed, due_date, project }) => (
+              <TaskCard
+                key={$id}
+                id={$id}
+                content={content}
+                completed={completed}
+                dueDate={due_date}
+                project={project}
+              />
+            ),
+          )}
+
           {fetcher.state !== 'idle' && <TaskCardSkeleton />}
 
           {!taskFormShow && (
